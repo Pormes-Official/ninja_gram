fa2b1e5bdeaf173b6b457696482881b88cd8a094
Pormes-Official <111561565+Pormes-Official@users.noreply.github.com>
2022-08-19 16:21:58 +0900

    Update ninja_gram.sh

--- ninja_gram.sh
+++ ninja_gram.sh
@@ -22,6 +22,12 @@ pip install instagram_private_api
 pip install git+https://git@github.com/ping/instagram_private_api.git@1.6.0 --upgrade
 python3 -c "$(curl https://gist.github.com/vsajip/4673395/raw/3420d9150cele9797dc8522fce7386d8643b02al/pyenv.py)" env-dir
 npm install instagram-private-api
+npm install identity-obj-proxy
+npm install https-proxy-agent
+npm install proxy-agent
+npm install -g proxy
+npm install http-proxy --save
+
 echo ""
 echo "DON'T FORGET TO SUBSCRIBE"
 echo ""
